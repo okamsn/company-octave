11da339211507e56ec28e5a0cdcdd864b12c27b2
okamsn <ej32u@protonmail.com>
2018-06-06 22:29:01 -0400

    Update company-octave.el
    Trying to get doc-buffers working.

--- company-octave.el
+++ company-octave.el
@@ -1,11 +1,12 @@
 ;; company mode for octave
+;; NOTE: The Octave REPL ("Inferior Octave") must be running.
 
 (require 'cl-lib)
 (require 'company)
 
 
 (defun company-octave-grab-prefix ()
-  "Get the completion target, including '.' for completing struct members. Octave REPL has a function 'completion_matches' to give candidates."
+  "Get the completion target, including '.' for completing struct members."
    (if (or (looking-at "\\_>") (looking-back "\\_>\\."))
       (buffer-substring-no-properties (point)
                                       (save-excursion
@@ -17,21 +18,44 @@
 
 (defun company-octave-get-candidates (prefix)
   "Use Octave REPL's 'completion_matches' function to get candidates."
+  (with-local-quit
   (inferior-octave-send-list-and-digest (list (concat "completion_matches ('" prefix "');\n")))
-  inferior-octave-output-list)
+  inferior-octave-output-list))
 
 (defun company-octave-get-annotation (prefix)
   "Use Octave REPL's 'which' command to get inline annotations. It tells us the type of the prefix."
+  (with-local-quit
   (inferior-octave-send-list-and-digest (list (concat "which ('" prefix "');\n")))
-  (concat " " (nth 3 (split-string (car inferior-octave-output-list) " "))))
+    (concat " " (nth 3 (split-string (car inferior-octave-output-list) " ")))
+  ))
 
-;; (defun company-octave-get-docbuffer (prefix)
-;;   )
+;; How does this differ from annotate?
+;; (defun company-octave-get-meta-info (prefix)
+;;   "Use Octave REPL's 'which' command to get inline annotations. It tells us the type of the prefix."
+;;   (with-local-quit
+;;   (inferior-octave-send-list-and-digest (list (concat "which ('" prefix "');\n")))
+;;   (car inferior-octave-output-list)))
 
+(defun company-octave-get-doc-buffer (prefix)
+  ;; get basic info on prefix
+  (inferior-octave-send-list-and-digest (list (concat "which ('" arg "');\n")))
+  (pcase (nth 3 (split-string (car inferior-octave-output-list) " "))
+    ("function" (progn
+                  (inferior-octave-send-list-and-digest (list (concat "help ('" arg "');\n")))))
+    ("variable" (progn
+                  (inferior-octave-send-list-and-digest (list (concat "type ('" arg "', '-q');\n"))))))
+  ;; remove empty lines, separate what's left
+  (company-doc-buffer
+   (combine-and-quote-strings (cl-loop for candidate in inferior-octave-output-list
+                                       if (not (string-equal candidate ""))
+                                       collect candidate)
+                              "\n"))
+  (company-show-doc-buffer))
 
 (defun company-octave-backend (command &optional arg &rest ignored)
-  "Define the backend for interacting with the OCtave REPL."
+  "Define the backend for interacting with the Octave REPL."
   (interactive (list 'interactive))
+  (with-local-quit
   (cl-case command
     (interactive (company-begin-backend 'company-octave-backend))
     (prefix (if (eq major-mode 'octave-mode)
@@ -39,12 +63,13 @@
     (candidates (company-octave-get-candidates arg))
     (annotation (company-octave-get-annotation arg))
     ;;(init (inferior-octave))
+    (doc-buffer (company-octave-get-doc-buffer arg)))
     ))
 
 
 
 (defun company-octave-settings ()
-  (add-to-list 'company-backends '(company-octave-backend company-dabbrev-code company-files))
+  ;;(add-to-list 'company-backends '(company-octave-backend company-dabbrev-code company-files company-etags company-gtags company-keywords))
   (setq-mode-local octave company-dabbrev-code-other-buffers 'code)
   ;; (setq-mode-local octave company-dabbrev-code-ignore-case t)
   )
