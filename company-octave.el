ba4ed5142029789714b4c3b63af1284350352928
okamsn <ej32u@protonmail.com>
2018-07-04 17:51:02 -0400

    Annotate now works with structs
    Returns nothing by necessity.

--- company-octave.el
+++ company-octave.el
@@ -7,7 +7,8 @@
 
 (defun company-octave-grab-prefix ()
   "Get the completion target, including '.' for completing struct members."
-   (if (or (looking-at "\\_>") (looking-back "\\_>\\."))
+  ;; (interactive)
+  (if (or (looking-at "\\_>") (looking-back "\\_>\\."))
       (buffer-substring-no-properties (point)
                                       (save-excursion
                                         (skip-chars-backward "a-zA-Z_.0-9")
@@ -18,16 +19,20 @@
 
 (defun company-octave-get-candidates (prefix)
   "Use Octave REPL's 'completion_matches' function to get candidates."
+  ;; (interactive)
   (with-local-quit
   (inferior-octave-send-list-and-digest (list (concat "completion_matches ('" prefix "');\n")))
   inferior-octave-output-list))
 
 (defun company-octave-get-annotation (prefix)
-  "Use Octave REPL's 'which' command to get inline annotations. It tells us the type of the prefix."
+  "Use Octave REPL's 'which' command to get inline annotations. It tells us the type of the prefix. There's an error with completing structure names, so it just doesn't."
   (with-local-quit
-  (inferior-octave-send-list-and-digest (list (concat "which ('" prefix "');\n")))
-    (concat " " (nth 3 (split-string (car inferior-octave-output-list) " ")))
-  ))
+    (if (string-match-p ".*\\..*" arg)
+        nil
+      (inferior-octave-send-list-and-digest (list (concat "which ('" prefix "');\n")))
+      (concat " " (nth 3 (split-string (car inferior-octave-output-list) " ")))
+      ))
+  )
 
 ;; How does this differ from annotate?
 ;; (defun company-octave-get-meta-info (prefix)
@@ -36,21 +41,21 @@
 ;;   (inferior-octave-send-list-and-digest (list (concat "which ('" prefix "');\n")))
 ;;   (car inferior-octave-output-list)))
 
-(defun company-octave-get-doc-buffer (prefix)
-  ;; get basic info on prefix
-  (inferior-octave-send-list-and-digest (list (concat "which ('" arg "');\n")))
-  (pcase (nth 3 (split-string (car inferior-octave-output-list) " "))
-    ("function" (progn
-                  (inferior-octave-send-list-and-digest (list (concat "help ('" arg "');\n")))))
-    ("variable" (progn
-                  (inferior-octave-send-list-and-digest (list (concat "type ('" arg "', '-q');\n"))))))
-  ;; remove empty lines, separate what's left
-  (company-doc-buffer
-   (combine-and-quote-strings (cl-loop for candidate in inferior-octave-output-list
-                                       if (not (string-equal candidate ""))
-                                       collect candidate)
-                              "\n"))
-  (company-show-doc-buffer))
+;; (defun company-octave-get-doc-buffer (prefix)
+;;   ;; get basic info on prefix
+;;   (inferior-octave-send-list-and-digest (list (concat "which ('" arg "');\n")))
+;;   (pcase (nth 3 (split-string (car inferior-octave-output-list) " "))
+;;     ("function" (progn
+;;                   (inferior-octave-send-list-and-digest (list (concat "help ('" arg "');\n")))))
+;;     ("variable" (progn
+;;                   (inferior-octave-send-list-and-digest (list (concat "type ('" arg "', '-q');\n"))))))
+;;   ;; remove empty lines, separate what's left
+;;   (company-doc-buffer
+;;    (combine-and-quote-strings (cl-loop for candidate in inferior-octave-output-list
+;;                                        if (not (string-equal candidate ""))
+;;                                        collect candidate)
+;;                               "\n"))
+;;   (company-show-doc-buffer))
 
 (defun company-octave-backend (command &optional arg &rest ignored)
   "Define the backend for interacting with the Octave REPL."
@@ -62,15 +67,15 @@
                 (company-octave-grab-prefix)))
     (candidates (company-octave-get-candidates arg))
     (annotation (company-octave-get-annotation arg))
-    ;;(init (inferior-octave))
-    (doc-buffer (company-octave-get-doc-buffer arg)))
-    ))
-
+    (init (run-octave))
+    ;; (doc-buffer (company-octave-get-doc-buffer arg)))
+    )))
 
 
 (defun company-octave-settings ()
-  ;;(add-to-list 'company-backends '(company-octave-backend company-dabbrev-code company-files company-etags company-gtags company-keywords))
-  (setq-mode-local octave company-dabbrev-code-other-buffers 'code)
+  "Set up company with the appropriate backends. You might wish to change this."
+  (add-to-list 'company-backends 'company-octave-backend)
+  (setq company-dabbrev-code-other-buffers 'code)
   ;; (setq-mode-local octave company-dabbrev-code-ignore-case t)
   )
 
